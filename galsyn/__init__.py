4840a0b003d946e3933429fd1161946fcdf2b9c4
aabdurrouf <fabdurr1@jhu.edu>
2025-07-26 21:54:41 -0400

    update __init__.py

--- galsyn/__init__.py
+++ galsyn/__init__.py
@@ -6,7 +6,8 @@ from . import galsyn_run_fsps
 from . import galsyn_run_bagpipes
 from . import galsyn
 from .galsyn import GalaxySynthesizer
+from .sfh import SFHReconstructor
 
-__all__ = ['config', 'utils', 'imgutils', 'simutils_tng', 'galsyn_run_fsps', 'galsyn_run_bagpipes', 'galsyn']
+__all__ = ['config', 'utils', 'imgutils', 'simutils_tng', 'galsyn_run_fsps', 'galsyn_run_bagpipes', 'galsyn', 'sfh']
 
 __version__ = "0.1.0"
