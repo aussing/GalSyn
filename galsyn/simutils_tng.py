a680f1b02984ffc9e1ffc2c625f72dc66d860680
aabdurrouf <fnuabdur@iu.edu>
2026-02-07 20:42:26 -0500

    updated simutils_tng.py module

--- galsyn/simutils_tng.py
+++ galsyn/simutils_tng.py
@@ -266,19 +266,29 @@ def make_sim_file_from_tng_data(input_hdf5, z, cosmo_h=0.6774, XH=0.76, output_h
     stars_vel = stars_vel[idx,:]
 
     # get gas particles data
-    gas_mass = f['PartType0']['Masses'][:] * 1e+10 / cosmo_h
-    gas_zmet = f['PartType0']['GFM_Metallicity'][:]
-    gas_sfr_inst = f['PartType0']['StarFormationRate'][:]   # in Msun/yr
-    u = f['PartType0']['InternalEnergy'][:]
-    Xe = f['PartType0']['ElectronAbundance'][:]
-    gamma = 5.0/3.0
-    KB = 1.3807e-16
-    mp = 1.6726e-24
-    mu = (4*mp)/(1 + (3*XH) + (4*XH*Xe))
-    gas_temp = (gamma-1.0)*(u/KB)*mu*1e+10
-    gas_coords = f['PartType0']['Coordinates'][:] * snap_a / cosmo_h   # in kpc
-    gas_vel = f['PartType0']['Velocities'][:] * np.sqrt(snap_a)   # peculiar velocity in km/s
-    gas_mass_H = gas_mass * XH
+    if 'PartType0' in f:
+        gas_mass = f['PartType0']['Masses'][:] * 1e+10 / cosmo_h
+        gas_zmet = f['PartType0']['GFM_Metallicity'][:]
+        gas_sfr_inst = f['PartType0']['StarFormationRate'][:]   # in Msun/yr
+        u = f['PartType0']['InternalEnergy'][:]
+        Xe = f['PartType0']['ElectronAbundance'][:]
+        gamma = 5.0/3.0
+        KB = 1.3807e-16
+        mp = 1.6726e-24
+        mu = (4*mp)/(1 + (3*XH) + (4*XH*Xe))
+        gas_temp = (gamma-1.0)*(u/KB)*mu*1e+10
+        gas_coords = f['PartType0']['Coordinates'][:] * snap_a / cosmo_h   # in kpc
+        gas_vel = f['PartType0']['Velocities'][:] * np.sqrt(snap_a)   # peculiar velocity in km/s
+        gas_mass_H = gas_mass * XH
+
+    else:
+        gas_mass = [0]
+        gas_zmet = [0]
+        gas_sfr_inst = [0]
+        gas_temp = [0]
+        gas_coords = np.zeros((1,3))
+        gas_vel = np.zeros((1,3))
+        gas_mass_H = [0]
     
     f.close()
 
